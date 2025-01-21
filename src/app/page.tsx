187efaa73a2b29ad153c582d08d26d69339596a2
Sami Fawcett <samifawcett.nyc@gmail.com>
2025-01-20 18:31:21 -0800

    add grid for langtonloop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,22 @@
+import Grid from '@/components/grid';
 import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen gap-4">
-      <Link href="/writings" className="text-white hover:text-gray-300">
+    <div className="relative flex flex-col items-center justify-center min-h-screen gap-4">
+      <Link href="/writings" className="text-white hover:text-gray-300 z-10">
         writings
       </Link>
-      <Link href="/thoughts" className="text-white hover:text-gray-300">
+      <Link href="/thoughts" className="text-white hover:text-gray-300 z-10">
         thoughts
       </Link>
-      <Link href="/programs" className="text-white hover:text-gray-300">
+      <Link href="/programs" className="text-white hover:text-gray-300 z-10">
         programs
       </Link>
+      <Grid
+      rows={10} 
+      cols={10}>
+      </Grid>
     </div>
   );
 }
---
+++ src/components/grid.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import { useImmer } from 'use-immer';
+import './components.css';
+
+interface GridProps {
+    rows: number;
+    cols: number;
+}
+
+export default function Grid({rows, cols}: GridProps) {
+    const [grid, setGrid] = useImmer(Array.from({ length: rows }, () => Array(cols).fill(0)));
+
+    function updateCell(row: number, col: number, value: number) {
+        setGrid(draft => {
+            draft[row][col] = value;
+        });
+    }
+
+    return (
+        <div>
+            {grid.map((row, rowIndex) => (
+                <div key={rowIndex} className="row">
+                    {row.map((cell, colIndex) => (
+                        <button onClick={e => {
+                            updateCell(rowIndex, colIndex, grid[rowIndex][colIndex] + 1);
+                        }}
+                        key={colIndex}
+                        className="cell">
+                            {cell}
+                        </button>
+                    ))}
+                </div>
+            ))}
+        </div>
+    )
+}
