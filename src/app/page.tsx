e5db5062cb3391f1cafb9c59e58a54efe477b634
Sami Fawcett <samifawcett.nyc@gmail.com>
2025-05-25 00:22:15 -0700

    replicators added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Button from '@/components/btn';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import LangtonLoops from '@/components/langton-loops';
+import LangtonLoops from '@/components/Lreplicator';
 
 export default function Home() {
 
