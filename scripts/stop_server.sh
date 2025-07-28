0e9a44f31d26f74fe8b50d92f564c72ce3d4ff16
Sami Fawcett <samifawcett.nyc@gmail.com>
2025-07-27 19:10:10 -0700

    change to 80 and let ALB terminate the connection for TLS

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 if [ -f "$HOME/.nvm/nvm.sh" ]; then
-  . "$HOME/.nvm/nvm.sh"
-  nvm use 22
+    . "$HOME/.nvm/nvm.sh"
+    nvm use 22
 fi
-pm2 stop app-443 || true
+
 pm2 stop app-80 || true
 pm2 stop app || true
