f528ce17ea57766d29159992860d988b043026c2
Sami Fawcett <34167607+samif0@users.noreply.github.com>
2025-08-02 17:01:07 -0700

    Restore translucent nav with scroll transition and replicator

--- src/components/home-background.tsx
+++ src/components/home-background.tsx
@@ -3,7 +3,13 @@
 import dynamic from 'next/dynamic';
 
 const Boids = dynamic(() => import('./boids'), { ssr: false });
+const Lreplicator = dynamic(() => import('./Lreplicator'), { ssr: false });
 
 export default function HomeBackground() {
-  return <Boids />;
+  return (
+    <div className="hidden sm:block">
+      <Boids />
+      <Lreplicator />
+    </div>
+  );
 }
